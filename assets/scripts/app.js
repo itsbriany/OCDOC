0ffe8818062c22ac70e3db615776da7b3b43a5da
orangepek0e <georgejohnston13@hotmail.com>
2016-01-30 01:08:33 -0400

    your move, kid

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,12 +1,12 @@
 'use strict';
-angular.module('OfficeCrawler', ['ngRoute'])
+angular.module('OfficeCrawler', ['ngRoute', 'angular-carousel'])
 
     .config(function ($routeProvider) {
         $routeProvider
             .when('/', {
-                templateUrl: '../../views/profile.html'
+                templateUrl: '../../views/profile.html',
             })
             .otherwise({
-                redirectTo: '../../views/profile.html'
+                redirectTo: '/'
             });
     });
