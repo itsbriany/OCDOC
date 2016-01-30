30e292d5784c946a1c67e2f2d9594e0998e81e5a
Brian Yip <itsbriany@gmail.com>
2016-01-30 15:10:39 -0400

    Merging

--- tests/dbtest.php
+++ tests/dbtest.php
@@ -3,6 +3,9 @@
   require_once '../connection.php';
   require_once '../controllers/dbmanager.php';
 
+  global $conn;
+
+  var_dump($conn);
 
   $dbManager = new DBManager($conn);
   $dbManager.fetchTaskList(1);
