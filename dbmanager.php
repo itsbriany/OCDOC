6c9f599d5dbc8fe02138f0e03bc271cb8aedcd1c
christopher dwyer-perkins <KaanuMac@christophers-MacBook-Air.local>
2016-01-30 11:44:18 -0400

    dbman

--- dbmanager.php
+++ dbmanager.php
@@ -17,16 +17,27 @@ class DBManager {
 
   // Check connection
   if ($conn->connect_error) {
-    return ("Connection failed: " . $conn->connect_error) . "</br>";
+    die ("Connection failed: " . $conn->connect_error) . "</br>";
   }
   echo "Connected successfully</br>";
 
+  $conn->close();
+
   public function setPlayer($id) {
+
+    $sql = "SELECT PlayerID FROM Players WHERE PlayerID = " . $id . ";";
+    $result = $conn->query($sql);
+
+    if ($result->num_rows > 0) {
+      $this->playerID = (int)$row["PlayerID"];
+    }
+
+    return $this->platerID;
 
   }
 
   public function getTurnId() {
-
+    $sql = "SELECT "
   }
 
   public function getTODO() {
